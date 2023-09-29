58b7b2d2900bf8fab2d44c468743fdaddc0a07b0
Darshan Brahmbhatt <145489542+darshan-brahmbhatt@users.noreply.github.com>
2023-09-29 19:11:50 -0400

    Ways of Refer to Html

---
+++ Javascript_Refer_To_Html/ReferByTagName.js
@@ -0,0 +1,7 @@
+function picture(){
+    let d = document.getElementsByTagName("img")
+    alert("Number of Images are "+d.length)
+}
+
+// check ReferByTagname file for how to refer by tag name 
+// you also can do this with embbeded javascrip
